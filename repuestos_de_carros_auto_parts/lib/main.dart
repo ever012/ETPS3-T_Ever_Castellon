5cf694b0ab44f7564c166ca8e59556baf483bf0f
Ever Enrique Castellon Mazariego <2505672018@mail.utec.edu.sv>
2023-11-03 21:49:22 -0600

    modificando nombre de la app

--- repuestos_de_carros_auto_parts/lib/main.dart
+++ repuestos_de_carros_auto_parts/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Repuestos de Carros Auto Parts',
+      title: 'Auto Parts',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
