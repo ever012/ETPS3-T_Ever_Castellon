e6f42999aca5c2b70c88e9fe70afe50a53a89472
Ever Enrique Castellon Mazariego <2505672018@mail.utec.edu.sv>
2023-11-30 22:25:05 -0600

    actualizando app

--- repuestos_de_carros_auto_parts/android/app/src/main/java/utec/edu/sv/repuestos_de_carros_auto_parts/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package utec.edu.sv.repuestos_de_carros_auto_parts;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- repuestos_de_carros_auto_parts/lib/api_config.dart
+++
@@ -1,4 +0,0 @@
-class ApiConfig {
-  static const String apiUrl = 'http://192.168.1.3:8083/';
-  //static const String apiUrl = 'https://apirepuestoscarros20231028004808.azurewebsites.net/';
-}
